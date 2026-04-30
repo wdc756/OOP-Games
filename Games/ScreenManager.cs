11ce7b9fbcddaabc293b2b0661b91ab24be86328
wdc <wdc23a@acu.edu>
2026-04-30 12:45:29 -0500

    Improved screen manager

--- Games/ScreenManager.cs
+++ Games/ScreenManager.cs
@@ -32,11 +32,23 @@ namespace Games
             Console.Write(" ");
         }
         
-        // Use this to set a given point to the string provided
-        public static void DrawPoint(int x, int y, string s)
+        // Use this to set a given point to the char provided
+        public static void SetPoint(int x, int y, char c)
         {
             if (x < 0 || x >= GetScreenWidth() || y < 0 || y >= GetScreenHeight())
                 throw new IndexOutOfRangeException($"Can't draw point {x},{y} - out of range {GetScreenWidth()},{GetScreenHeight()}");
+            
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
+        
+        // Use this to print a string starting at a given coordinate
+        public static void Print(int x, int y, string s)
+        {
+            if (x < 0 || x >= GetScreenWidth() || y < 0 || y >= GetScreenHeight())
+                throw new IndexOutOfRangeException($"Can't print at {x},{y} - out of range {GetScreenWidth()},{GetScreenHeight()}");
+            if (x + s.Length > GetScreenWidth())
+                throw new ArgumentException($"Cannot print string with length {s.Length} at {x} - out of range {GetScreenWidth()}");
             
             Console.SetCursorPosition(x, y);
             Console.Write(s);
